fc2d2fbde91ed041f756783fc099d5616ecb7e8e
Georg Meinhardt <habanzu@gmail.com>
2022-04-29 09:43:20 +0200

    Work on passing graph to C Code

--- Labelling/cpp_lib.cpp
+++ Labelling/cpp_lib.cpp
@@ -1,11 +1,15 @@
 #include <vector>
 #include "cpp_lib.h"
 
-std::vector<myProcessor> myProcessors;
+using std::vector;
 
-myProcessor::myProcessor(int exp_in, int size_in){
-    this->exp = exp_in;
-    this->size = size_in;
+vector<myProcessor> myProcessors;
+vector<int> nodes;
+vector<vector<double> > edges;
+
+myProcessor::myProcessor(int size, double* data){
+    this->size = size;
+    this->data = data;
 }
 
 int myProcessor::process(double *d, int size){
@@ -15,9 +19,9 @@ int myProcessor::process(double *d, int size){
     return 0;
 }
 
-unsigned int myProcessorInit(int exp_in, int size_in){
+unsigned int myProcessorInit(int size, double* data){
     unsigned int id;
-    myProcessor tmpInst(exp_in, size_in);
+    myProcessor tmpInst(size, data);
     myProcessors.push_back(tmpInst);
     id = myProcessors.size() -1;
     return id;
@@ -25,4 +29,15 @@ unsigned int myProcessorInit(int exp_in, int size_in){
 
 int myProcessorProcess(unsigned int id, double*d, int size){
     return myProcessors[id].process(d,size);
+}
+
+void initGraph(unsigned num_nodes, int* node_data, double* edge_data){
+    for(unsigned i=0;i<num_nodes;i++){
+        nodes.push_back(node_data[i]);
+        vector<double> v;
+        for(unsigned j=0;j<num_nodes;j++){
+            v.push_back(edge_data[i*num_nodes + j]);
+        }
+        edges.push_back(v);
+    }
 }
--- Labelling/cpp_lib.h
+++ Labelling/cpp_lib.h
@@ -1,14 +1,15 @@
 class myProcessor {
 protected:
-    int exp;
     int size;
+    double* data;
 
 public:
-    myProcessor(int exp_in, int size_in);
+    myProcessor(int size, double* data);
     int process(double *d, int size);
 };
 
 extern "C" {
-    unsigned int myProcessorInit(int exp_in, int size_in);
+    void initGraph(unsigned num_nodes, int* node_data, double* edge_data);
+    unsigned int myProcessorInit(int size, double* data);
     int myProcessorProcess(unsigned int id, double *d, int size);
 } //end extern "C"
