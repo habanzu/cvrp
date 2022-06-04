6de20379e11e443dbdc3db0c5e8214c95bce400e
Georg Meinhardt <habanzu@gmail.com>
2022-06-04 23:26:14 +0200

    Plot the results of the bounds at the same time

--- pricer.py
+++ pricer.py
@@ -15,10 +15,14 @@ ffi.cdef(funDefs, override=True)
 
 class VRPPricer(Pricer):
     def pricerinit(self):
+        if 'methods' not in self.data:
+            raise ValueError("The method(s) of the pricer need to be specified.")
+        self.data['bounds'] = {}
+        for method in self.data['methods']:
+            self.data['bounds'][method] = []
+
         self.data['cons'] = [self.model.getTransformedCons(con) for con in self.model.cons]
         self.data['vars'] = {path:self.model.getTransformedVar(var) for (path,var) in self.model.vars.items()}
-        if 'ngParam' not in self.data:
-            self.data['ngParam'] = 1
 
         demands = list(nx.get_node_attributes(self.model.graph,"demand").values())
         if not np.all(np.array(demands[1:])):
@@ -36,35 +40,78 @@ class VRPPricer(Pricer):
         num_nodes = ffi.cast("unsigned",self.model.graph.number_of_nodes())
 
         capacity_ptr = ffi.cast("double",self.data['capacity'])
-        labelling_lib.initGraph(num_nodes,nodes_arr,edges_arr, capacity_ptr, self.data['max_path_len'], self.data['ngParam'])
+        ngParam = 8
+        print(f"PY PRICING: The neighborhood has been fixed to {ngParam} neighbors.")
+        labelling_lib.initGraph(num_nodes,nodes_arr,edges_arr, capacity_ptr, self.data['max_path_len'], ngParam)
 
     def init_data(self, G):
         self.data = {}
         self.data["capacity"] = G.graph['capacity']
         self.data["num_vehicles"] = int(G.graph['min_trucks'])
-        self.data['bounds'] = []
 
     def pricerfarkas(self):
         dual = [self.model.getDualfarkasLinear(con) for con in self.data['cons']]
 #         print(f"PRICER_PY: Farkas Values are {dual}")
-        return self.labelling(dual, farkas=True)
+        return self.SPPRC_chooser(dual, farkas=True)
 
     def pricerredcost(self):
         dual = [self.model.getDualsolLinear(con) for con in self.data['cons']]
 #         print(f"PRICER_PY: Dual variables are {dual}")
-        return self.labelling(dual)
+        return self.SPPRC_chooser(dual, farkas=False)
 
-    def labelling(self, dual,farkas=False, elementary=False, max_vars=10000, cyc2=False, abort_early=False, ngParam = 1, ngPath = False):
-        if 'elementary' in self.data:
-            elementary = self.data['elementary']
-        if 'max_vars' in self.data:
-            max_vars = self.data['max_vars']
-        if 'cyc2' in self.data:
-            cyc2 = self.data['cyc2']
-        if 'abort_early' in self.data:
-            abort_early = self.data['abort_early']
-        if 'ngPath' in self.data:
-            ngPath = self.data['ngPath']
+    def addVar(self, path, farkas):
+        if path in self.data['vars'].keys():
+            cost = self.model.getVarRedcost(self.data['vars'][path])
+            if farkas:
+                print(f"PY Farkaspricing: Path already exists. | {path}")
+            else:
+                print(f"PY Pricing: Path already exists. Reduced Cost {cost:.2f} | {path}")
+            raise NotImplementedError("Path already exists. This is probably due to a missing branching rule.")
+
+        weight = nx.path_weight(self.model.graph,path,"weight")
+
+        var = self.model.addVar(vtype="C",obj=weight,pricedVar=True)
+
+        counts = np.unique(path[1:-1], return_counts=True)
+        for i, node in enumerate(counts[0]):
+            self.model.addConsCoeff(self.data['cons'][node-1], var ,counts[1][i])
+
+        self.model.addConsCoeff(self.data['cons'][-1], var, 1)
+        self.data['vars'][tuple(path)] = var
+        return var
+
+    def SPPRC_chooser(self, dual, farkas):
+        max_vars = self.data['max_vars']
+        abort_early = self.data['abort_early']
+
+        for i, method in enumerate(self.data['methods']):
+            if method == 'elementary':
+                paths, upper_bound, lower_bound = self.labelling(dual,farkas,True,max_vars,False,abort_early,False)
+            elif method == 'ng8':
+                paths, upper_bound, lower_bound = self.labelling(dual,farkas,False,max_vars,False,abort_early,True,8)
+            elif method == 'ng20':
+                raise NotImplementedError("At the moment the ng Parameter is fixed to 8.")
+                paths, upper_bound, lower_bound = self.labelling(dual,farkas,False,max_vars,False,abort_early,True,20)
+            elif method == 'cyc2':
+                paths, upper_bound, lower_bound = self.labelling(dual,farkas,False,max_vars,True,abort_early,False)
+            elif method == 'SPPRC':
+                paths, upper_bound, lower_bound = self.labelling(dual,farkas,False,max_vars,False,abort_early,False)
+            else:
+                raise ValueError("Method in pricerdata methods does not exist.")
+            if not farkas:
+                self.data['bounds'][method].append((upper_bound,lower_bound))
+            if i == 0:
+                lowerbound = lower_bound
+                for path in paths:
+                    self.addVar(path,farkas)
+
+
+        if not farkas:
+            return {'result':SCIP_RESULT.SUCCESS,'lowerbound':lowerbound}
+        else:
+            return {'result':SCIP_RESULT.SUCCESS}
+
+    def labelling(self, dual,farkas, elementary, max_vars, cyc2, abort_early, ngParam, ngPath=0):
 
         # TODO: Possible improvement: result can be reused every time
         pointer_dual = ffi.cast("double*", np.array(dual,dtype=np.double).ctypes.data)
@@ -72,46 +119,34 @@ class VRPPricer(Pricer):
         result = np.zeros(max_vars*self.data['max_path_len'] ,dtype=np.uintc)
         result_arr = ffi.cast("unsigned*",result.ctypes.data)
 
-        num_paths = labelling_lib.labelling(pointer_dual, farkas, elementary, max_vars, cyc2, result_arr, abort_early, ngPath)
-        print(f"PY PRICING: Found {num_paths} paths with reduced cost")
+        num_paths = labelling_lib.labelling(pointer_dual, farkas, elementary, max_vars, cyc2, result_arr, abort_early, ngParam)
+        # print(f"PY PRICING: Found {num_paths} paths with reduced cost")
 
+        upper_bound = self.model.getObjVal()
         if(num_paths == 0):
-            print("PY PRICING: There are no paths with negative reduced costs")
-            return {'result':SCIP_RESULT.SUCCESS}
+            # print("PY PRICING: There are no paths with negative reduced costs")
+            if not farkas:
+                return [], upper_bound, upper_bound
+            else:
+                return [], 0 , 0
 
         lowest_cost = 0
+        paths = []
         for i in range(min(num_paths,max_vars)):
             single_result = result[i*self.data['max_path_len']:(i+1)*self.data['max_path_len']]
             result_indices = np.insert(np.nonzero(single_result),0,0)
             result_indices = np.append(result_indices,0)
             path = tuple(single_result[result_indices])
-
-            if path in self.data['vars'].keys():
-                cost = self.model.getVarRedcost(self.data['vars'][path])
-                if farkas:
-                    print(f"PY Farkaspricing: Path already exists. | {path}")
-                else:
-                    print(f"PY Pricing: Path already exists. Reduced Cost {cost:.2f} | {path}")
-                return {'result':SCIP_RESULT.SUCCESS}
-
+            paths.append(path)
             weight = nx.path_weight(self.model.graph,path,"weight")
 
-            var = self.model.addVar(vtype="C",obj=weight,pricedVar=True)
             if not farkas:
                 red_cost = weight - sum([dual[i-1] for i in path[1:-1]]) - dual[-1]
                 if red_cost < lowest_cost:
                     lowest_cost = red_cost
 
-            counts = np.unique(path[1:-1], return_counts=True)
-            for i, node in enumerate(counts[0]):
-                self.model.addConsCoeff(self.data['cons'][node-1], var ,counts[1][i])
-
-            self.model.addConsCoeff(self.data['cons'][-1], var, 1)
-            self.data['vars'][tuple(path)] = var
-
         if not farkas:
-            upper_bound = self.model.getObjVal()
             lower_bound = upper_bound + self.data['num_vehicles']*lowest_cost
-            self.data['bounds'].append((upper_bound,lower_bound))
-            return {'result':SCIP_RESULT.SUCCESS,'lowerbound':lower_bound}
-        return {'result':SCIP_RESULT.SUCCESS}
+            return paths, upper_bound, lower_bound
+        else:
+            return paths, 0 , 0
