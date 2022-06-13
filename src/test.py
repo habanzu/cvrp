c4877e03c84a0bc5d8577563a74c33f1c466cca5
Georg.Meinhardt <Georg.Meinhardt@uni-bonn.de>
2022-06-13 12:31:03 +0200

    Add test.py file

--- src/test.py
+++ src/test.py
@@ -4,22 +4,23 @@ from model import VRP, create_constraints, output_variables, create_example_2
 from pricer import VRPPricer
 from parse import parse
 
-
-G = parse("Instances/E/E-n23-k3.vrp")
+Instance = "E/E-n23-k3"
+G = parse(f"Instances/{Instance}.vrp")
 
 model = VRP(G)
 
 # Create pricer
 pricer = VRPPricer()
 pricer.init_data(G)
-pricer.data['methods'] = ["SPPRC"]
+pricer.data['methods'] = ["ESPPRC"]
 pricer.data['max_vars']= 10000
-pricer.data['time_limit'] = 60
+pricer.data['time_limit'] = 3600
+print(f"Time limit is {pricer.data['time_limit']}")
 pricer.data['farley'] = False
 
 model.includePricer(pricer, "pricer","does pricing")
 
-create_constraints(model,G,heuristic_time=0)
+create_constraints(model,G,heuristic_stale_it=100, heuristic_max_it=2e4)
 
 model.optimize()
 model.hideOutput(quiet=False)
