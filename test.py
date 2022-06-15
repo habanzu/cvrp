77196bd136d60b7417874e5b0b8fbe919d3f64b7
Georg.Meinhardt <Georg.Meinhardt@uni-bonn.de>
2022-06-15 12:18:40 +0200

    Add nfs files to gitignore

--- test.py
+++ test.py
@@ -2,20 +2,20 @@ from src.model import VRP, create_constraints, output_variables
 from src.pricer import VRPPricer
 from src.parse import parse
 
-Instance = "X-n172-k51"
-G = parse(Instance, 53)
+Instance = "X-n502-k39"
+G = parse(Instance, 39)
 
 model = VRP(G)
 
 # Create pricer
 pricer = VRPPricer(G)
-pricer.data['methods'] = ["ESPPRC","ng8","cyc2","SPPRC"]
-pricer.data['max_vars']= 1
-pricer.data['time_limit'] = 10
+pricer.data['methods'] = ["ESPPRC"]
+pricer.data['max_vars']= 1000
+pricer.data['time_limit'] = 86400
 pricer.data['farley'] = False
 
 model.includePricer(pricer, "pricer","does pricing")
-create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e5, heuristic_time=1e-5)
+create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e6, heuristic_time=1e-3)
 
 model.optimize()
 model.hideOutput(quiet=False)
