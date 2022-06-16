c392c1c6f5bee3852f9024f94e223bac38e93a58
Georg Meinhardt <habanzu@gmail.com>
2022-06-16 17:15:16 +0200

    Improve adaptive heuristic

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ model = VRP(G)
 pricer = VRPPricer(G)
 pricer.data['methods'] = ['ng8']
 pricer.data['max_vars']= 1000
-pricer.data['time_limit'] = 3600
+pricer.data['time_limit'] = 60
 pricer.data['farley'] = False
 
 model.includePricer(pricer, "pricer","does pricing")
-create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e3, heuristic_time=1e-3)
+create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e7, heuristic_time=1e-5)
 
 model.optimize()
 model.hideOutput(quiet=False)
