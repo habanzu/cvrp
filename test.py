50e96a62baa0409fd0a4ead9bdc5f2bdb6e82e8e
Georg Meinhardt <habanzu@gmail.com>
2022-06-16 16:35:18 +0200

    Add new results for ng

--- test.py
+++ test.py
@@ -3,20 +3,20 @@ from src.pricer import VRPPricer
 from src.parse import parse
 from src.output import write_solution
 
-Instance = "E-n22-k4"
-G = parse(Instance)
+Instance = "X-n120-k6"
+G = parse(Instance, 6)
 
 model = VRP(G)
 
 # Create pricer
 pricer = VRPPricer(G)
-pricer.data['methods'] = ["ESPPRC"]
+pricer.data['methods'] = ['ng8']
 pricer.data['max_vars']= 1000
-pricer.data['time_limit'] = 86400
+pricer.data['time_limit'] = 3600
 pricer.data['farley'] = False
 
 model.includePricer(pricer, "pricer","does pricing")
-create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e7, heuristic_time=1e-5)
+create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e3, heuristic_time=1e-3)
 
 model.optimize()
 model.hideOutput(quiet=False)
