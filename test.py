f0bb1ecd1dad2a13e1330286ec1913d8ed73469f
Georg Meinhardt <habanzu@gmail.com>
2022-06-17 17:38:13 +0200

    Add test.py again

---
+++ test.py
@@ -0,0 +1,24 @@
+from src.model import VRP, create_constraints, output_variables
+from src.pricer import VRPPricer
+from src.parse import parse
+from src.output import write_solution
+
+Instance = "E-n22-k4"
+G = parse(Instance)
+
+model = VRP(G)
+
+# Create pricer
+pricer = VRPPricer(G)
+pricer.data['methods'] = [method]
+pricer.data['max_vars']= int(1e6)
+pricer.data['time_limit'] = 86400
+pricer.data['farley'] = False
+
+model.includePricer(pricer, "pricer","does pricing")
+create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e8, heuristic_time=1e-5)
+
+model.hideOutput()
+model.optimize()
+
+write_solution(model, pricer)
