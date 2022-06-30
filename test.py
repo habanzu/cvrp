43036efd3b5a234ab33aea2c7ae8cc0944b09c9e
Georg.Meinhardt <Georg.Meinhardt@uni-bonn.de>
2022-06-30 11:15:58 +0200

    Add used config files

--- test.py
+++ test.py
@@ -3,20 +3,20 @@ from src.pricer import VRPPricer
 from src.parse import parse
 from src.output import write_solution
 
-Instance = "E-n23-k3"
-G = parse(Instance)
+Instance = "X-n186-k15"
+G = parse(Instance, 15,filename=f"output_uchoa/{Instance}-ng20")
 
 model = VRP(G)
 
 # Create pricer
 pricer = VRPPricer(G)
 pricer.data['methods'] = ["ng20"]
-pricer.data['max_vars']= int(1e6)
-pricer.data['time_limit'] = 10
+pricer.data['max_vars']= 10000
+pricer.data['time_limit'] = 86400
 pricer.data['farley'] = False
 
 model.includePricer(pricer, "pricer","does pricing")
-create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e6, heuristic_time=1e-5)
+create_constraints(model,pricer,heuristic_stale_it=20, heuristic_max_it=2e4, heuristic_time=1)
 
 model.hideOutput(quiet=True)
 model.optimize()
