fb7a8d74034c090ec98d124fb1b705ff5dc6d070
Georg Meinhardt <habanzu@gmail.com>
2022-07-11 14:37:55 +0200

    further evalutation

--- src/parse.py
+++ src/parse.py
@@ -109,12 +109,21 @@ def parse_output(file):
     header = parse_header(file)
     return pd.read_csv(file, header=header ,skipfooter=footer, engine="python", sep=", ")
 
-# def parse_unfinished(file):
-#     footer = 2
-#     header = parse_header(file)
-#     if footer <= 0:
-#         print(f"ERROR: Footer is {footer}")
-#     return pd.read_csv(file, header=header ,skipfooter=footer, engine="python", sep=", ")
+def parse_sol_val(Instance):
+    if Instance[0] == "E":
+        dir = "E"
+    elif Instance[0] == "X":
+        dir = "Uchoa"
+    else:
+        raise ValueError("Instance Class is not known to the parser.")
+    file = f"Instances/{dir}/{Instance}.sol"
+
+    with open(file,'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if line.startswith("Cost"):
+            return line.split(" ")[1]
 
 
 def log_finished(file):
